df976cfde94566ece437ef38302a6f8b6f26af1b
LIXDEVV <jorgeleython2@gmail.com>
2026-04-23 13:14:37 -0500

    Actualización del proyecto - [describe brevemente qué cambiaste]

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export const metadata: Metadata = {
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '[url=https://postimages.org/][img]https://i.postimg.cc/xq3g0gvs/favicon.jpg[/img][/url]',
   },
 }
 
